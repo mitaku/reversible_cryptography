8c02bded8db4baafda9e17152e32fbb40c8360e2
gussan <egussan@gmail.com>
2015-03-31 12:46:53 +0900

    Add cli for encryption and decryption

---
+++ lib/reversible_cryptography/cli.rb
@@ -0,0 +1,26 @@
+require 'thor'
+require 'reversible_cryptography'
+
+module ReversibleCryptography
+  class CLI < Thor
+    desc "encrypt [TEXT]", "Encrypt text"
+    option :password, type: :string, aliases: [:p]
+    def encrypt(plain_text=nil)
+      plain_text ||= ask("Input text:")
+      password = options[:password]
+      password ||= ask("Input password:", echo: false).tap { puts }
+
+      puts ReversibleCryptography::Message.encrypt(plain_text, password)
+    end
+
+    desc "encrypt [TEXT]", "Decrypt text"
+    option :password, type: :string, aliases: [:p]
+    def decrypt(encrypted_text=nil)
+      encrypted_text ||= ask("Input text:")
+      password = options[:password]
+      password ||= ask("Input password:", echo: false).tap { puts }
+
+      puts ReversibleCryptography::Message.decrypt(encrypted_text, password)
+    end
+  end
+end
